1526a54b3c8eb57d90abef14b8b9a02a5eb21188
MATSUMOTO Eiji <e-matsumoto@esm.co.jp>
2013-04-11 11:45:06 +0900

    Finish user edit, update, index, and destroy acctions

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,11 @@
 module UsersHelper
-  def gravatar_for(user)
+  def gravatar_for(user, options=nil)
   	gravatar_id  = Digest::MD5::hexdigest(user.email.downcase)
   	gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-  	image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  	if options.nil?
+	  image_tag(gravatar_url, alt: user.name, class: "gravatar")
+	else
+	  image_tag(gravatar_url, alt: user.name, width: options[:size], height: options[:size], class: "gravatar")
+	end
   end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-  	name     "Michael Hartl"
-  	email    "michael@example.com"
+  	sequence(:name)  { |n| "Persion #{n}" }
+  	sequence(:email) { |n| "persion_#{n}@example.com" }
   	password "foobar"
   	password_confirmation "foobar"
+
+  	factory :admin do
+  	  admin true
+  	end
   end
 end
