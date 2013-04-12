fccf326a93f5854d0528ffe05356b6640808bb0e
MATSUMOTO Eiji <e-matsumoto@esm.co.jp>
2013-04-12 11:27:43 +0900

    Add user microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,6 +5,8 @@ describe "Static pages" do
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   before { visit root_path }
 
+  subject { page }
+
   describe "Home page" do
     it "should have the h1 'Sample App'" do
       page.should have_selector('h1', :text => 'Sample App')
@@ -16,6 +18,22 @@ describe "Static pages" do
 
     it "should not have a custon page title" do
       page.should_not have_selector('title', :text => '| Home')
+    end
+
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          page.should have_selector("li##{item.id}", text: item.content)
+        end
+      end
     end
   end
 
